08247b7ebcfe94f7473661b6ac30687c0d4123b3
outclassstudio <mlimshs@gmail.com>
2022-06-29 21:10:17 +0900

    feat: set query

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,30 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      title
+      id
+      small_cover_image
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie details</div>;
+  const { id } = useParams();
+  const { data, loading, error } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+  console.log(data);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Could not recieve</div>;
+  }
+  return <div>{data.movie.title}</div>;
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,29 +1,67 @@
-import { gql, useApolloClient } from "@apollo/client";
+import { gql, useApolloClient, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      title
+      id
+    }
+    allTweets {
+      id
+      text
+    }
+  }
+`;
 
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
+  // const [movies, setMovies] = useState([]);
+  // const client = useApolloClient();
 
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              title
-            }
-          }
-        `,
-      })
-      .then((result) => setMovies(result.data.allMovies));
-  }, [client]);
+  // useEffect(() => {
+  //   client
+  //     .query({
+  //       query: gql`
+  //         {
+  //           allMovies {
+  //             title
+  //           }
+  //         }
+  //       `,
+  //     })
+  //     .then((result) => setMovies(result.data.allMovies));
+  // }, [client]);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Could not fetch :(</div>;
+  }
   return (
-    <div>
-      {movies.map((movie) => {
-        return <li key={movie.id}>{movie.title}</li>;
+    <ul>
+      <h1>Movies</h1>
+      {data.allMovies.map((el) => {
+        return (
+          <li key={el.id}>
+            <Link to={`/movies/${el.id}`}>{el.title}</Link>
+          </li>
+        );
       })}
-    </div>
+      <h1>Tweets</h1>
+      {data.allTweets.map((el) => {
+        return <li key={el.id}>{el.text}</li>;
+      })}
+    </ul>
   );
+
+  // return (
+  //   <div>
+  //     {movies.map((movie) => {
+  //       return <li key={movie.id}>{movie.title}</li>;
+  //     })}
+  //   </div>
+  // );
 }
