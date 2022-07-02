1059ba7a62d5570075fb48f30be1e9750b17eaf8
outclassstudio <mlimshs@gmail.com>
2022-07-02 13:42:26 +0900

    use cache fragment

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -6,19 +6,41 @@ const GET_MOVIE = gql`
     movie(id: $movieId) {
       title
       id
+      rating
       small_cover_image
+      isLiked @client
     }
   }
 `;
 
 export default function Movie() {
   const { id } = useParams();
-  const { data, loading, error } = useQuery(GET_MOVIE, {
+  const {
+    data,
+    loading,
+    error,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
     variables: {
       movieId: id,
     },
   });
   console.log(data);
+
+  //복습필수
+  const onClick = () => {
+    cache.writeFragment({
+      id: `Movie:${id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        isLiked: !data.movie.isLiked,
+      },
+    });
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -26,5 +48,14 @@ export default function Movie() {
   if (error) {
     return <div>Could not recieve</div>;
   }
-  return <div>{data.movie.title}</div>;
+  return (
+    <>
+      <div>{data.movie.title}</div>
+      <div>{data.movie?.rating}</div>
+      <img src={data.movie?.small_cover_image} />
+      <button onClick={onClick}>
+        {data.movie?.isLiked ? "unlike" : "like"}
+      </button>
+    </>
+  );
 }
